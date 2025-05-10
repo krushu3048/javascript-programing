d3ea5208aa20f08b030fc8a15c83806959db0611
krushu3048 <krushnalp92@gmail.com>
2025-05-10 21:35:40 +0530

    String Basic Assignment
    String Basic Assignment

---
+++ 03-stringBasicAssig01.js
@@ -0,0 +1,17 @@
+function stringBasics() {
+  
+  console.log(`My dream company is "Google"`);
+
+  
+   var hobby1 = "Reading";
+   var hobby2 = "Cycling";
+   var hobby3 = "Traveling";
+
+  
+  console.log(`My hobbies are: ${hobby1}, ${hobby2}, ${hobby3}`);
+
+
+  let totalLength = hobby1.length + hobby2.length + hobby3.length;
+  console.log(`Total number of characters in hobbies: ${totalLength}`);
+}
+stringBasics();
