ebfa4a30e2b70e28d1b6c3eceb4ad3e2ea57d49d
krushu3048 <krushnalp92@gmail.com>
2025-05-19 15:09:37 +0530

    for loop Assignment
    for loop Assignment

---
+++ 06-for-assignment01.js
@@ -0,0 +1,55 @@
+console.log(`==================5 To 15 By Increment====================`);
+
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+  
+}
+
+
+console.log(`===============50 To 40 Decrement=================`);
+
+for (let i = 50; i >= 40; i--) {
+    console.log(i);
+}
+
+console.log(`==================15 ODD Numbers==================`);
+
+for (let index = 1; index <30; index++) {
+    if (index % 2 != 0 ) {
+        console.log(index);
+    }
+}
+
+
+
+console.log(`====================10 Even  Number=======================`);
+for (let index = 1; index <=20; index++) {
+   if (index % 2 ==0) {
+console.log(index);
+
+   }
+    
+}
+
+console.log(`====================Print the table 5=======================`);
+
+for (let index = 1; index <=10; index++) {
+    var result=index*5;
+   console.log(result);
+   
+    
+}
+console.log(`====================Print the table 10=======================`);
+for (let index = 1; index <=10; index++) {
+    var result = index*10
+    console.log(result);
+    
+    
+}
+console.log(`====================Print the table 10 reverse order=======================`);
+for (let index = 10; index >=1; index--) {
+   var result= index*10
+    console.log(result);
+    
+    
+}
